b895fabcd076756744d91a3539a22edb5d53193c
Dan Loewenherz <dloewenherz@gmail.com>
2012-12-02 21:13:54 -0800

    display error message when attempting to use Style parameter

--- bottlenose/api.py
+++ bottlenose/api.py
@@ -57,14 +57,14 @@ class AmazonError(Exception):
 
 class AmazonCall(object):
     def __init__(self, AWSAccessKeyId=None, AWSSecretAccessKey=None, \
-            AssociateTag=None, Operation=None, Style=None, Version=None, \
-            Region=None, Timeout=None):
+            AssociateTag=None, Operation=None, Version=None, Region=None, \
+            Timeout=None):
+
         self.AWSAccessKeyId = AWSAccessKeyId
         self.AWSSecretAccessKey = AWSSecretAccessKey
         self.Operation = Operation
         self.AssociateTag = AssociateTag
         self.Version = Version
-        self.Style = Style
         self.Region = Region
         self.Timeout = Timeout
 
@@ -77,10 +77,13 @@ class AmazonCall(object):
         except:
             return AmazonCall(self.AWSAccessKeyId, self.AWSSecretAccessKey, \
                     self.AssociateTag, Operation=k, Version=self.Version,
-                    Style=self.Style, Region=self.Region, Timeout=self.Timeout)
+                    Region=self.Region, Timeout=self.Timeout)
 
     def __call__(self, **kwargs):
         logger = logging.getLogger(__name__)
+
+        if kwargs['Style'] != None:
+            raise AmazonError("The `Style` parameter has been discontinued by AWS. Please remove all references to it and reattempt your request.")
 
         kwargs['Timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
         kwargs['Operation'] = self.Operation
@@ -88,16 +91,10 @@ class AmazonCall(object):
         kwargs['AWSAccessKeyId'] = self.AWSAccessKeyId
         kwargs['Service'] = "AWSECommerceService"
 
-        if self.Style:
-            kwargs['Style'] = self.Style
-
         if self.AssociateTag:
             kwargs['AssociateTag'] = self.AssociateTag
 
-        if 'Style' in kwargs:
-            service_domain = SERVICE_DOMAINS[self.Region][1]
-        else:
-            service_domain = SERVICE_DOMAINS[self.Region][0]
+        service_domain = SERVICE_DOMAINS[self.Region][0]
 
         keys = kwargs.keys()
         keys.sort()
@@ -127,10 +124,11 @@ class AmazonCall(object):
 
 class Amazon(AmazonCall):
     def __init__(self, AWSAccessKeyId=None, AWSSecretAccessKey=None, \
-            AssociateTag=None, Operation=None, Style=None, \
-            Version="2011-08-01", Region="US", Timeout=None):
+            AssociateTag=None, Operation=None, Version="2011-08-01", Region="US", \
+            Timeout=None):
+
         AmazonCall.__init__(self, AWSAccessKeyId, AWSSecretAccessKey, \
-            AssociateTag, Operation, Version=Version, Region=Region, Style=Style, Timeout=Timeout)
+            AssociateTag, Operation, Version=Version, Region=Region, Timeout=Timeout)
 
 __all__ = ["Amazon", "AmazonError"]
 
