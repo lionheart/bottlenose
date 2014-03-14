61b6529a683d17f8e924b9e29cb72694c6a2d8e0
David Marin <dm@davidmarin.org>
2014-03-14 16:55:38 -0700

    moved URL forming out of __call__()

--- bottlenose/api.py
+++ bottlenose/api.py
@@ -55,6 +55,21 @@ SERVICE_DOMAINS = {
 }
 
 
+def _quote_query(query):
+    """Turn a dictionary into a query string in a URL, with keys
+    in alphabetical order."""
+    if sys.version_info[0] == 3:
+        return "&".join("%s=%s" % (
+            k, urllib.parse.quote(
+                str(query[k]).encode('utf-8'), safe='~'))
+                for k in sorted(query))
+    else:
+        return "&".join("%s=%s" % (
+            k, urllib.quote(
+                unicode(query[k]).encode('utf-8'), safe='~'))
+                for k in sorted(query))
+
+
 class AmazonError(Exception):
     pass
 
@@ -63,6 +78,7 @@ class AmazonCall(object):
     def __init__(self, AWSAccessKeyId=None, AWSSecretAccessKey=None,
             AssociateTag=None, Operation=None, Version=None, Region=None,
             Timeout=None, MaxQPS=None, Parser=None,
+            CacheReader=None, CacheWriter=None,
             _last_query_time=None):
 
         self.AWSAccessKeyId = (AWSAccessKeyId or
@@ -71,6 +87,8 @@ class AmazonCall(object):
                                    os.environ.get('AWS_SECRET_ACCESS_KEY'))
         self.AssociateTag = (AssociateTag or
                              os.environ.get('AWS_ASSOCIATE_TAG'))
+        self.CacheReader = CacheReader
+        self.CacheWriter = CacheWriter
         self.MaxQPS = MaxQPS
         self.Operation = Operation
         self.Parser = Parser
@@ -93,51 +111,37 @@ class AmazonCall(object):
                               Operation=k, Version=self.Version,
                               Region=self.Region, Timeout=self.Timeout,
                               MaxQPS=self.MaxQPS, Parser=self.Parser,
+                              CacheReader=self.CacheReader,
+                              CacheWriter=self.CacheWriter,
                               _last_query_time=self._last_query_time)
 
-    def __call__(self, **kwargs):
-        log = logging.getLogger(__name__)
+    def _maybe_parse(self, response_text):
+        if self.Parser:
+            return self.Parser(response_text)
+        else:
+            return response_text
 
-        if 'Style' in kwargs:
-            raise AmazonError("The `Style` parameter has been discontinued by"
-                              " AWS. Please remove all references to it and"
-                              " reattempt your request.")
 
-        def quote_query(query):
-            if sys.version_info[0] == 3:
-                return "&".join("%s=%s" % (
-                    k, urllib.parse.quote(
-                        str(query[k]).encode('utf-8'), safe='~'))
-                        for k in sorted(query))
-            else:
-                return "&".join("%s=%s" % (
-                    k, urllib.quote(
-                        unicode(query[k]).encode('utf-8'), safe='~'))
-                        for k in sorted(query))
+    def api_url(self, **kwargs):
+        """The URL for making the given query against the API."""
+        query = {
+            'Operation': self.Operation,
+            'Service': "AWSECommerceService",
+            'Timestamp': time.strftime(
+                "%Y-%m-%dT%H:%M:%SZ", time.gmtime()),
+            'Version': self.Version,
+        }
+        query.update(kwargs)
 
-        kwargs['Timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%SZ",
-                                            time.gmtime())
-        kwargs['Operation'] = self.Operation
-        kwargs['Version'] = self.Version
-        kwargs['AWSAccessKeyId'] = self.AWSAccessKeyId
-        kwargs['Service'] = "AWSECommerceService"
+        query['AWSAccessKeyId'] = self.AWSAccessKeyId
+        query['Timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%SZ",
+                                           time.gmtime())
 
         if self.AssociateTag:
-            kwargs['AssociateTag'] = self.AssociateTag
+            query['AssociateTag'] = self.AssociateTag
 
         service_domain = SERVICE_DOMAINS[self.Region][0]
-
-        if self.MaxQPS:
-            last_query_time = self._last_query_time[0]
-            if last_query_time:
-                wait_time = 1 / self.MaxQPS - (time.time() - last_query_time)
-                if wait_time > 0:
-                    log.debug('Waiting %.3fs to call Amazon API' % wait_time)
-                    time.sleep(wait_time)
-
-            self._last_query_time[0] = time.time()
-
-        quoted_strings = quote_query(kwargs)
+        quoted_strings = _quote_query(query)
 
         data = "GET\n" + service_domain + "\n/onca/xml\n" + quoted_strings
 
@@ -150,8 +154,51 @@ class AmazonCall(object):
             digest = hmac.new(self.AWSSecretAccessKey, data, sha256).digest()
             signature = urllib.quote(b64encode(digest))
 
-        api_url = ("http://" + service_domain + "/onca/xml?" +
-                      quoted_strings + "&Signature=%s" % signature)
+        return ("http://" + service_domain + "/onca/xml?" +
+                quoted_strings + "&Signature=%s" % signature)
+
+    def cache_url(self, **kwargs):
+        """A simplified URL to be used for caching the given query."""
+        query = {
+            'Operation': self.Operation,
+            'Service': "AWSECommerceService",
+            'Version': self.Version,
+        }
+        query.update(kwargs)
+
+        service_domain = SERVICE_DOMAINS[self.Region][0]
+
+        return "http://" + service_domain + "/onca/xml?" + _quote_query(query)
+
+    def __call__(self, **kwargs):
+        log = logging.getLogger(__name__)
+
+        if 'Style' in kwargs:
+            raise AmazonError("The `Style` parameter has been discontinued by"
+                              " AWS. Please remove all references to it and"
+                              " reattempt your request.")
+
+        cache_url = self.cache_url(**kwargs)
+
+        if self.CacheReader:
+            cached_response_text = self.CacheReader(cache_url)
+            if cached_response_text is not None:
+                return self._maybe_parse(cached_response_text)
+
+        api_url = self.api_url(**kwargs)
+
+        # throttle ourselves if need be
+        if self.MaxQPS:
+            last_query_time = self._last_query_time[0]
+            if last_query_time:
+                wait_time = 1 / self.MaxQPS - (time.time() - last_query_time)
+                if wait_time > 0:
+                    log.debug('Waiting %.3fs to call Amazon API' % wait_time)
+                    time.sleep(wait_time)
+
+            self._last_query_time[0] = time.time()
+
+        # make the actual call
         api_request = urllib2.Request(
             api_url, headers={"Accept-Encoding": "gzip"})
         if self.Timeout:
@@ -161,6 +208,7 @@ class AmazonCall(object):
         if self.Timeout:
             socket.setdefaulttimeout(None)
 
+        # decompress the response if need be
         if sys.version_info[0] == 3:
             if "gzip" in response.info().get("Content-Encoding"):
                 response_text = gzip.decompress(response.read())
@@ -173,21 +221,31 @@ class AmazonCall(object):
             else:
                 response_text = response.read()
 
-        if self.Parser:
-            return self.Parser(response_text)
-        else:
-            return response_text
+        # write it back to the cache
+        if self.CacheWriter:
+            # put this in a dictionary so we can add fields as needed
+            response_info = {
+                'data': response_text,
+                'status_code': response.getcode(),
+            }
+            self.CacheWriter(cache_url, response_info)
+
+        # parse and return it
+        return self._maybe_parse(response_text)
 
 
 class Amazon(AmazonCall):
     def __init__(self, AWSAccessKeyId=None, AWSSecretAccessKey=None,
             AssociateTag=None, Operation=None, Version="2011-08-01",
-            Region="US", Timeout=None, MaxQPS=None, Parser=None):
+            Region="US", Timeout=None, MaxQPS=None, Parser=None,
+            CacheReader=None, CacheWriter=None):
 
         AmazonCall.__init__(self, AWSAccessKeyId, AWSSecretAccessKey,
                             AssociateTag, Operation, Version=Version,
                             Region=Region, Timeout=Timeout,
-                            MaxQPS=MaxQPS, Parser=Parser)
+                            MaxQPS=MaxQPS, Parser=Parser,
+                            CacheReader=CacheReader,
+                            CacheWriter=CacheWriter)
 
 
 __all__ = ["Amazon", "AmazonError"]
