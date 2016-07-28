091340ede95f1b7ecf589adc943c770f62485844
Dan Loewenherz <dan@lionheartsw.com>
2016-07-28 10:42:54 -0500

    add metadata file

---
+++ bottlenose/metadata.py
@@ -0,0 +1,21 @@
+# Copyright 2012 Lionheart Software LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = "0.6.4"
+__author__ = "Dan Loewenherz"
+__copyright__ = "Copyright 2012 Lionheart Software LLC"
+__maintainer__ = "Dan Loewenherz"
+__email__ = "dan@lionheartsw.com"
+__license__ = "Apache 2.0"
+
